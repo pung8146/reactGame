93f6096bc365c8d201c93ad26b46e21c01d4d6d4
pung8146 <pung8146@naver.com>
2021-11-08 21:12:59 +0900

    WordRelay Jsx form 1

--- 1.gugudan/GuGuDan.jsx
+++ 1.gugudan/GuGuDan.jsx
@@ -1,48 +1,28 @@
 const React = require('react');
-const { useState, useRef } = React;
+const { Component } = React;
 
-const GuGuDan = () => {
-    const [first, setFirst] = useState(
-      Math.ceil(Math.random() * 9)
-    );
-    const [second, setSecond] = useState(
-      Math.ceil(Math.random() * 9)
-    );
-    const [value, setValue] = useState("");
-    const [result, setResult] = useState("");
-    const inputRef = React.useRef(null);
+class WordRelay extends Component {
+  state = {
+    word : "박상훈",
+    value : '',
+    result : '',
+  }
 
-    const onSubmitForm = (e) => {
-      e.preventDefault();
-      if (parseInt(value) === first * second) {
-        setResult("정답");
-        setFirst(Math.ceil(Math.random() * 9));
-        setSecond(Math.ceil(Math.random() * 9));
-        setValue("");
-        inputRef.current.focus();
-      } else {
-        setResult("땡");
-        setValue("");
-        inputRef.current.focus();
-      }
-    };
 
-   const onChangeInput = (e) => {
-      setValue(e.target.value);
-    };
+onSubmitFomr = () => {
 
-    return (
-      <>
-        <div>
-          {first} 곱하기 {second} 는 ?
-        </div>
-        <form onSubmit={onSubmitForm}>
-          <input ref={inputRef} onChange={onChangeInput} value={value} />
-          <button>입력!</button>
-        </form>
-        <div id="result">{result}</div>
-      </>
-    );
-  };
+};
 
-  module.exports = GuGuDan;
+render () {
+  return (
+  <>
+    <div>{this.state.word}</div>
+    <form onSubmit={this.onSubmitForm}> 
+      <input ref={this.onRefInput} value={this.state.value} onChange={this.onChangeInput} />
+      <button>입력!</button>
+    </form>
+  </>
+  );
+}
+}
+module.exports = WordRelay;
