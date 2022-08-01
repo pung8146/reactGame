94af082add8554242be9bc97ad9456a0556978a4
pung8146 <pung8146@naver.com>
2022-08-01 09:58:04 +0900

    webpack hot loading 을 추가합니다 .  데이터를 유지합니다!

--- 2.WordRelay/webpack.config.js
+++ 2.WordRelay/webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const RefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin')
 
 module.exports = {
     name: 'wordrelay-setting',
@@ -25,12 +26,28 @@ module.exports = {
                 }],
                  '@babel/preset-react'
                 ],
-                plugins: ['@babel/plugin-proposal-class-properties'],
+                plugins: [
+                    '@babel/plugin-proposal-class-properties',
+                    'react-refresh/babel',
+                ],
             },
         }],
     },
+    
+    plugins: [
+        new RefreshWebpackPlugin()
+    ],
+
     output : {
         path: path.join(__dirname, 'dist'), // dist 는 현재폴더에서 만들어줍니다.
         filename: 'app.js',
-    } // 출력
+        publicPath: '/dist/',
+    },
+    devServer: {
+        devMiddleware: { publicPath: '/dist' },
+        static: { directory: path.resolve(__dirname) },
+        hot: true
+      }
+      
+    // 출력
 }
