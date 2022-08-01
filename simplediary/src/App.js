992b5969bce8c462c638bc5ffa3ca6597a99b8b2
pung8146 <pung8146@naver.com>
2022-08-02 08:52:54 +0900

    SimpleDiary 컴포넌트를 생성했습니다.

---
+++ simplediary/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import DiaryEditor from './DirayEditor';
+
+function App() {
+  return (
+    <div className="App">
+      <h2>일기장</h2>
+      <DiaryEditor />
+    </div>
+  );
+}
+
+export default App;
